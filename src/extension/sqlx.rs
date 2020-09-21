e5826a99db976c0209bc756cd579d18ae6cbfe65
Timofey Martynov <feymartynov@users.noreply.github.com>
2020-09-21 21:21:37 +0300

    Add sqlx extension (#9)

---
+++ src/extension/sqlx.rs
@@ -0,0 +1,17 @@
+use sqlx::Error;
+use http::StatusCode;
+
+use crate::Error as TheError;
+
+impl From<Error> for TheError {
+    fn from(value: Error) -> TheError {
+        let status = match &value {
+            Error::RowNotFound => StatusCode::NOT_FOUND,
+            _ => StatusCode::UNPROCESSABLE_ENTITY,
+        };
+
+        let mut err = TheError::from(status);
+        err.set_detail(&format!("a database error = '{}'", &value));
+        err
+    }
+}
